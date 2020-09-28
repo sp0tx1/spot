b1cb139c7ca4b51e647f0c50deb46505958cbf19
carlosromero1232929292 <71998258+carlosromero1232929292@users.noreply.github.com>
2020-09-27 20:16:43 -0700

    Update authorize.js

--- authorize.js
+++ authorize.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
   let client_id = '0e84c5db823e4272b080a0cecab13393';
   // Use the following site to convert your regular url to the encoded version: 
   // https://www.url-encode-decode.com/
-  let redirect_uri = '';
+  let redirect_uri = 'https%3A%2F%2Fsp0tx1.github.io%2Fspot%2F';
   // *************** END *************************
   const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&scope=user-read-playback-state%20user-read-playback-position%20user-read-email%20user-library-read%20user-top-read%20playlist-modify-public%20user-follow-read%20user-read-currently-playing%20user-read-recently-played%20user-read-private%20playlist-read-private%20user-library-modify%20playlist-read-collaborative%20playlist-modify-private%20user-follow-modify%20user-modify-playback-state&state=pk23cs&response_type=token&redirect_uri=${redirect_uri}`;
   console.log(`redirect url: ${redirect}`);
